d2c4f8bf6a908c4f55a50c2e4e8f4f97fac40aaa
Harshatha <harshatha3111@gmail.com>
2025-04-19 16:25:13 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const navbar = document.getElementById("navbar");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 50) {
-    navbar.style.background = "rgba(0, 0, 0, 0.8)";
-  } else {
-    navbar.style.background = "rgba(0, 0, 0, 0.5)";
-  }
-});
